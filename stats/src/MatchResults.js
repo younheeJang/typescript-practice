3834d435541efad40e5f8f68adcaa1095be87a05
younheeJang <edam7635@gmail.com>
2021-05-25 01:29:07 +0900

    /practice: add generics.ts

---
+++ stats/src/MatchResults.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.__esModule = true;
+exports.MatchResult = void 0;
+//enum - enumeration
+var MatchResult;
+(function (MatchResult) {
+    MatchResult["HomeWin"] = "H";
+    MatchResult["AwayWin"] = "A";
+    MatchResult["Draw"] = "D";
+})(MatchResult = exports.MatchResult || (exports.MatchResult = {}));
