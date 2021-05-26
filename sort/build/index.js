8e3b4c2d8e11f6d1c6081eebeb0ca4cccedc9e18
younheeJang <edam7635@gmail.com>
2021-05-26 23:14:58 +0900

    /practice: decorators.ts

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,16 +1,1 @@
-"use strict";
-exports.__esModule = true;
-var LinkedList_1 = require("./LinkedList");
-// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-// numbersCollection.sort();
-// console.log(numbersCollection.data);
-// const charactersCollection = new CharactersCollection('Xaayb');
-// charactersCollection.sort();
-// console.log(charactersCollection.data);
-var linkedList = new LinkedList_1.LinkedList();
-linkedList.add(500);
-linkedList.add(-10);
-linkedList.add(-3);
-linkedList.add(4);
-linkedList.sort();
-linkedList.print();
+console.log('hh');
