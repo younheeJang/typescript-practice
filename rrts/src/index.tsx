43e1833b8184aa1ea77cf61d0f4edc9a74021ff4
younheeJang <edam7635@gmail.com>
2021-05-27 18:46:54 +0900

    /rrts: react with typescript

---
+++ rrts/src/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+interface AppProps {
+  color: string;
+}
+
+// const App = (props: AppProps): JSX.Element => {
+//   return <div>{props.color}</div>;
+// };
+
+class App extends React.Component<AppProps> {
+  state = { counter: 0 };
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App color="blue" />, document.querySelector('#root'));
